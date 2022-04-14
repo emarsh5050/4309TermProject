3756f435456b27201e1f8f7e924e401403fe9f4d
Emma Marshall <emarsh5050@gmail.com>
2022-04-14 17:33:10 -0500

    added sidebar to menu-cart page

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.display === "flex") {
+        if (content.style.display === "block") {
         content.style.display = "none";
         } else {
-        content.style.display = "flex";
+        content.style.display = "block";
         }
     });
     }
